58b6c762c182aa6cdda6355ce55f6dc9f7948205
MadPlayer <kch9001@gmail.com>
2024-10-14 23:04:25 +0900

    update emacs local setting

---
+++ .local-dir.el
@@ -0,0 +1,9 @@
+((c-mode
+  (indent-tabs-mode . nil)
+  (c-file-style . "gnu")
+  (c-basic-offset . 2))
+ (c++-mode
+  (indent-tabs-mode . nil)
+  (c-file-style . "gnu")
+  (c-basic-offset . 2))
+ )
